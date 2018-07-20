36df412ef06892c3b6cf710b77b3b19356dc950a
lucasjukr94 <lucasjukr94@gmail.com>
2018-07-20 14:29:15 -0300

    scaffold

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body{
+	font-family:Calibri;
+	margin:0;
+	padding:0;
+}
+
+.exterior{
+	background-color:lightgray;
+	width:100%;
+	height:100%;
+}
+
+.content{
+	width:70%;
+	margin-left:15%;
+}
+
+.header{
+	height:20vh;
+	background-color:blue;
+}
+
+.body1{
+	height:80vh;
+	background-color:red;
+}
+
+.body2{
+	height:100vh;
+	background-color:pink;
+}
+
+.footer{
+	height:30vh;
+	background-color:black;
+}
+</style>
+</head>
+<body>
+	<div class="exterior">
+		<div class="content">
+			<div class="header">
+			</div>
+			<div class="body1">
+			</div>
+			<div class="body2">
+			</div>
+			<div class="footer">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
